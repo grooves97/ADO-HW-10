3383af2cd5ea4aaf7b0855c6b3993c0e41e3bfd9
Aseka <grooves97@gmail.com>
2019-04-25 23:08:51 +0600

    Проблема вывода на экран с жалобой отношения к объекту

---
+++ MusicGroup/MusicGroup.DataAcces/DataContextServices.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicGroup.Models;
+
+namespace MusicGroup.DataAcces
+{
+    public class DataContextServices
+    {
+        public void AddGroup(MusicCrew musicGroup)
+        {
+            using (var context = new DataContext())
+            {
+                context.MusicCrews.Add(musicGroup);
+                context.SaveChanges();
+            }
+        }
+        public void AddSong(Song song)
+        {
+            using (var context = new DataContext())
+            {
+                context.Songs.Add(song);
+                context.SaveChanges();
+            }
+        }
+
+        public List<MusicCrew> GetMusicGroups()
+        {
+            using (var context = new DataContext())
+            {
+                return context.MusicCrews.ToList();
+            }
+        }
+
+        public List<MusicCrew> GetSortedMusicGroups()
+        {
+            using (var context = new DataContext())
+            {
+                return context.MusicCrews.OrderBy(mus => mus.Rating).ToList();
+            }
+        }
+
+        public List<MusicCrew> GetDescendSortedMusicGroups()
+        {
+            using (var context = new DataContext())
+            {
+                return context.MusicCrews.OrderByDescending(mus => mus.Rating).ToList();
+            }
+        }
+
+        public List<Song> GetSongs()
+        {
+            using (var context = new DataContext())
+            {
+                return context.Songs.ToList();
+            }
+        }
+        public MusicCrew GetMusicGroupByGroupName(string groupName)
+        {
+            using (var context = new DataContext())
+            {
+                return context.MusicCrews.FirstOrDefault(msGr => msGr.Name == groupName);
+            }
+        }
+        public MusicCrew GetMusicGroupBySong(string songName)
+        {
+            using (var context = new DataContext())
+            {
+                return context.Songs.FirstOrDefault(song => song.Name == songName).MusicCrew;
+            }
+        }
+    }
+}
--- MusicGroup/MusicGroup/Program.cs
+++ MusicGroup/MusicGroup/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MusicGroup.DataAcces;
+using MusicGroup.Models;
 
 namespace MusicGroup
 {
@@ -10,7 +12,18 @@ namespace MusicGroup
     {
         static void Main(string[] args)
         {
+            MusicCrew musicCrew = new MusicCrew();
 
+            DataContextServices dataContextUseServices = new DataContextServices();
+
+            List<MusicCrew> musicGroups = dataContextUseServices.GetDescendSortedMusicGroups();
+
+            foreach (var group in musicGroups)
+            {
+                Console.WriteLine(group.Name);
+            }
+
+            Console.ReadLine();
         }
     }
 }
